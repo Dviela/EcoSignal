39bd68a505957a7aff0b443910451ea7108064e6
Dviela <a19230@svalero.com>
2026-02-06 12:29:17 +0100

    Clases de dominio

--- src/main/java/com/svalero/ecosignal/domain/Alerta.java
+++ src/main/java/com/svalero/ecosignal/domain/Alerta.java
@@ -1,4 +1,31 @@
 package com.svalero.ecosignal.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "alertas")
 public class Alerta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column
+    private String titulo;
+    @Column
+    private String descripcion;
+    @Column
+    private int nivel;
+    @Column
+    private double umbral;
+    @Column (name = "fecha_hora")
+    private LocalDate fechaHora;
+    @Column
+    private boolean resuelta = false;
+
 }
--- src/main/java/com/svalero/ecosignal/domain/Medicion.java
+++ src/main/java/com/svalero/ecosignal/domain/Medicion.java
@@ -1,4 +1,30 @@
 package com.svalero.ecosignal.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "medicion")
 public class Medicion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column
+    private String tipo;
+    @Column
+    private double valor;
+    @Column
+    private String unidad;
+    @Column(name = "fecha_hora")
+    private LocalDate fechaHora;
+    @Column
+    private boolean valida = true;
+
+
 }
