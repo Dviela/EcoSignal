03f8407946a3ec697ba2daac7358de431836cb75
Dviela <a19230@svalero.com>
2026-02-06 19:21:22 +0100

    Operaciones de exito 200 y 404

--- src/main/java/com/svalero/ecosignal/controller/MedicionController.java
+++ src/main/java/com/svalero/ecosignal/controller/MedicionController.java
@@ -5,6 +5,7 @@ import com.svalero.ecosignal.exception.MedicionNotFoundException;
 import com.svalero.ecosignal.service.MedicionService;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,14 +23,16 @@ public class MedicionController {
        @GetMapping("/medicion/{id}")
        public ResponseEntity<Medicion> get(@PathVariable long id) throws MedicionNotFoundException{
             Medicion medicion = medicionService.findById(id);
-            return ResponseEntity.ok(medicion);
+            return ResponseEntity.ok(medicion); //200 ok
         }
 
-        @PostMapping("/medicion")
-        public void addMedicion(@RequestBody Medicion medicion){
-            medicionService.add(medicion);
-        }
-        @PutMapping("/medicion/{id}")
+    @PostMapping("/medicion")
+    public ResponseEntity<Medicion> addMedicion(@RequestBody Medicion medicion){
+        Medicion nueva = medicionService.add(medicion);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nueva);   //201 created
+    }
+
+    @PutMapping("/medicion/{id}")
         public ResponseEntity<Medicion> modifyMedicion(@PathVariable long id, @RequestBody Medicion medicion) throws MedicionNotFoundException{
             Medicion newmedicion = medicionService.modify(id,medicion);
             return ResponseEntity.ok(newmedicion);
@@ -37,7 +40,7 @@ public class MedicionController {
         @DeleteMapping("/medicion/{id}")
         public ResponseEntity<Void> deleteMedicion(@PathVariable long id) throws MedicionNotFoundException {
             medicionService.delete(id);
-            return ResponseEntity.noContent().build();
+            return ResponseEntity.noContent().build(); //204 no content
         }
     }
 
--- src/main/java/com/svalero/ecosignal/exception/AlertaNotFoundException.java
+++ src/main/java/com/svalero/ecosignal/exception/AlertaNotFoundException.java
@@ -1,4 +1,8 @@
 package com.svalero.ecosignal.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class AlertaNotFoundException extends Exception{
 }
